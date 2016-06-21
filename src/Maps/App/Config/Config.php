626fe973ffa258e773d3267a2fda302317703e7b
Albo Vieira <albovieira@gmail.com>
2016-06-21 18:11:01 -0300

    Initial commit

---
+++ src/Maps/App/Config/Config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: albo-vieira
+ * Date: 21/06/16
+ * Time: 16:27
+ */
+
+namespace Maps\App\Config;
+
+class Config
+{
+    public static function config(){
+
+        return [
+            'url' => 'maps.google.com',
+            'key' => 'AIzaSyASlWsBkfJ8sX_uPQbAnYGlq32-uztwCbU',
+        ];
+
+    }
+}
